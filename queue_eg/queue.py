3443d6cc73aee67748323e1fcb5e25a754fa35bf
Bhushan Borse <borsebhushan216@gmail.com>
2025-07-09 15:17:44 +0530

    queue added

---
+++ queue_eg/queue.py
@@ -0,0 +1,33 @@
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.queue.pop(self.size() - 1)
+
+    def is_empty(self):
+        if self.queue:
+            return False
+        return True
+
+    def size(self):
+        return len(self.queue)
+
+    def __str__(self):
+        import json
+
+        return json.dumps(self.queue)
+
+
+if __name__ == "__main__":
+    queue = Queue()
+
+    queue.enqueue(10)
+    queue.enqueue(20)
+    queue.enqueue(30)
+    print(queue.dequeue())
+    print(queue)
