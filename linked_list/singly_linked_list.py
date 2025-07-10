2a6e5a1a167faafc1a3f9e95ea7f73c924ddcd90
Bhushan Borse <borsebhushan216@gmail.com>
2025-07-10 12:14:25 +0530

    insert_at_index

--- linked_list/singly_linked_list.py
+++ linked_list/singly_linked_list.py
@@ -11,8 +11,11 @@ class Node:
 class LinkList:
     def __init__(self):
         self.head: Node = None
+        self.length = 0
 
     def insert_at_end(self, data):
+
+        self.length += 1
 
         if self.head is None:
             self.head = Node(data=data)
@@ -27,8 +30,40 @@ class LinkList:
 
         return self
 
-    def insert_at_start(self):
-        pass
+    def insert_at_start(self, data):
+        self.length += 1
+        new_node = Node(data=data)
+        new_node.next_node = self.head
+        self.head = new_node
+        return self
+
+    def insert_at_index(self, index, data):
+
+        if index == 0:
+            self.insert_at_start(data)
+            return self
+
+        if index > (self.length + 1) or index < 0:
+            raise IndexError(f"The element cant be inserted at {index}")
+
+        if index == (self.length + 1):
+            self.insert_at_end(data)
+            return self
+
+        self.length += 1
+
+        new_node = Node(data=data)
+        current_node = self.head.next_node
+
+        current_index = 0
+        while current_index < index - 1:
+            current_node = current_node.next_node
+            current_index += 1
+
+        new_node.next_node = current_node.next_node
+        current_node.next_node = new_node
+
+        return self
 
     def display(self):
         current_node = self.head
@@ -46,4 +81,12 @@ class LinkList:
 if __name__ == "__main__":
 
     link_list_head = LinkList()
-    link_list_head.insert_at_end(10).insert_at_end(20).insert_at_end(30).display()
+    link_list_head.insert_at_end(10).insert_at_end(20).insert_at_end(
+        30
+    ).display().insert_at_start(0).display().insert_at_index(
+        0, -1
+    ).display().insert_at_index(
+        6, 40
+    ).display().insert_at_index(
+        2, 5
+    ).display()
